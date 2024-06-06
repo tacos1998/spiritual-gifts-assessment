b75729f087e3adb65e28f2b4dad06d4a8792ca2e
Tyler Acosta <tacos1998@gmail.com>
2024-06-06 11:34:20 -0500

    change img paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Quiz from './components/Quiz';
 function App() {
   return (
     <>
-      <a href='https://www.saintpeter.me' target="_blank"><img src="src/assets/stpeterlogotrimmed.png" alt="St. Peter Catholic Church Logo" width="350"></img></a>
+      <a href='https://www.saintpeter.me' target="_blank"><img src="stpeterlogotrimmed.png" alt="St. Peter Catholic Church Logo" width="350"></img></a>
       <h1>Spiritual Gifts Assessment</h1>
       <Quiz />
     </>
